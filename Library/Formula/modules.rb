c0d937f0dc8a4b47bb856df9a6168b5d0bbeee97
Chris Jubb <cjubb@makielab.com>
2014-07-29 16:51:03 +0100

    Set version to 0.1.0

--- Library/Formula/modules.rb
+++ Library/Formula/modules.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Modules < Formula
   homepage 'http://www.makielab.com'
   url 'git@github.com:makielab/modules.git', :using => :git
-  version '1.0.0'
+  version '0.1.0'
 
   def install
     system "xbuild /p:Configuration=Release modules.sln"
